02813f539e3a459cfd1a04fccd2afb43f008f634
Sreekanth <sreekanthps28@gmail.com>
2023-02-28 12:25:10 +0000

    Initial Commit

---
+++ SreekanthCard/SreekanthCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SreekanthCardApp.swift
+//  SreekanthCard
+//
+//  Created by SREEKANTH PS on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SreekanthCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
